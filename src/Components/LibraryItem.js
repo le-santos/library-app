c5dcc2a24224a545309294ed84ea79dd5ce37f82
le-santos <lequinterio@gmail.com>
2020-08-14 17:47:49 -0300

    Add new button to fetch composer data

--- src/Components/LibraryItem.js
+++ src/Components/LibraryItem.js
@@ -6,18 +6,37 @@ const ItemDiv = styled.div`
   padding: 1em;
   border: 1px solid #dee2e6;
   margin: 0.5em;
+  display: grid;
+  grid-template-areas: 
+    "title text delete"
+    "info link link";
+  place-items: center start;
+  gap: 10px;
+  box-shadow: 1px 2px 4px;
+  @media (max-width: 500px) {
+      display: grid;
+      grid-template-areas:
+        "title delete"
+        "text text"
+        "info link ";
+      place-items: center start;
+      gap: 10px;
+  }
 `;
+
+const titleStyle = {"gridArea": "title"},
+  textStyle = {"gridArea": "text"},
+  removeStyle = {"gridArea": "delete"},
+  infoStyle = {"gridArea": "info"},
+  linkStyle = {"gridArea": "link", "display": "inline-block"};
 
 function LibraryItem(props) {
   return (
     <ItemDiv>
-      <h3>{props.title}</h3>
-      <p>
-        <b>{"Music: "}</b>
-        {props.title}
-        <b>{", Composer: "}</b>
-        {props.composer}
-        <b>{", Style: "}</b>
+      <h3 style={titleStyle}>{props.title}</h3>
+      <p style={textStyle}><b>{"Composer: "}</b>
+        {props.composer},
+        <b>{" Style: "}</b>
         {props.style}
       </p>
       <Button
@@ -25,7 +44,22 @@ function LibraryItem(props) {
         color={props.childColor}
         clicked={props.removeClick}
         id={props.itemId}
+        style={removeStyle}
       ></Button>
+      <Button 
+        name={"Check Composer Info >>"} 
+        color={"#0c5460"} 
+        id={`btn-info-${props.itemId}`}
+        style={infoStyle}
+        clicked={props.requestData}
+      ></Button>
+      <a 
+        href={props.linkRef} 
+        id={`wikilink-${props.itemId}`}
+        style={linkStyle}
+      >
+        Wikipedia: {props.composer}
+      </a>
     </ItemDiv>
   );
 }
--- src/Components/LibraryList.js
+++ src/Components/LibraryList.js
@@ -1,17 +1,51 @@
 import React from "react";
 import LibraryItem from "./LibraryItem";
 
-const LibraryList = (props) =>
-  props.libraryList.map((item) => (
-    <LibraryItem
-      title={item.title}
-      composer={item.composer}
-      style={item.style}
-      key={item.id}
-      childColor={"#dc3545"}
-      removeClick={props.removeLibraryItem}
-      itemId={item.id}
-    />
-  ));
+const LibraryList = (props) => {
+  
+  const respLink = {link: "#"};
+  
+  const getData = (searchTerm) => {  
+    let url = "https://en.wikipedia.org/w/api.php";
+
+    const params = {
+      action: "opensearch",
+      search: searchTerm,
+      limit: "15",
+      namespace: "0",
+      format: "json"
+    };
+    
+    url = url + "?origin=*";
+
+    for (let key in params) {
+      url += `&${key}=${params[key]}`
+    }
+    
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {respLink.link = data[3][0]})
+      .catch((error) => console.log(error));
+
+    setTimeout( () =>{
+      console.log(respLink.link)},500);
+  }
+
+  return ( 
+    props.libraryList.map((item) => (
+      <LibraryItem
+        title={item.title}
+        composer={item.composer}
+        style={item.style}
+        key={item.id}
+        childColor={"#dc3545"}
+        removeClick={item.removeLibraryItem}
+        itemId={item.id}
+        requestData={() => getData(item.composer)}
+        linkRef={respLink.link}
+      />
+    ))
+  )    
+}
 
 export default LibraryList;
