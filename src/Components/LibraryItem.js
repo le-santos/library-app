d5e35c1dc8204f8581b607b70c320f1414a946ab
le-santos <lequinterio@gmail.com>
2020-08-17 14:21:16 -0300

    Fixed fetch function and useState hook in LibraryItem

--- src/Components/LibraryItem.js
+++ src/Components/LibraryItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Button from "./Button";
 
@@ -7,36 +7,89 @@ const ItemDiv = styled.div`
   border: 1px solid #dee2e6;
   margin: 0.5em;
   display: grid;
-  grid-template-areas: 
+  grid-template-areas:
     "title text delete"
     "info link link";
   place-items: center start;
   gap: 10px;
   box-shadow: 1px 2px 4px;
   @media (max-width: 500px) {
-      display: grid;
-      grid-template-areas:
-        "title delete"
-        "text text"
-        "info link ";
-      place-items: center start;
-      gap: 10px;
+    display: grid;
+    grid-template-areas:
+      "title delete"
+      "text text"
+      "info link ";
+    place-items: center start;
+    gap: 10px;
   }
 `;
 
-const titleStyle = {"gridArea": "title"},
-  textStyle = {"gridArea": "text"},
-  removeStyle = {"gridArea": "delete"},
-  infoStyle = {"gridArea": "info"},
-  linkStyle = {"gridArea": "link", "display": "none"};
+const titleStyle = { gridArea: "title" },
+  textStyle = { gridArea: "text" },
+  removeStyle = { gridArea: "delete" },
+  infoStyle = { gridArea: "info" },
+  linkStyle = { gridArea: "link" };
 
+let wikiInfo = { link: "#" };
+
+//function for fetching data from wikipedia
+const getData = async (searchTerm) => {
+  let url = "https://en.wikipedia.org/w/api.php";
+
+  const params = {
+    action: "opensearch",
+    search: searchTerm,
+    limit: "15",
+    namespace: "0",
+    format: "json",
+  };
+
+  url = url + "?origin=*";
+
+  for (let key in params) {
+    url += `&${key}=${params[key]}`;
+  }
+
+  await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      wikiInfo.link = data[3][0];
+    })
+    .catch((error) => console.log(error));
+
+  console.log(wikiInfo.link);
+  return wikiInfo.link;
+};
+
+//Lib Item Functional Component
 function LibraryItem(props) {
+  const [infoState, setInfoState] = useState({ visibility: false });
+
+  const toggleInfoState = () => {
+    setInfoState({ visibility: !infoState.visibility });
+  };
+
+  useEffect(() => {
+    console.log("Lib item mount or Update");
+    getData(props.composer);
+  }, [props.composer]);
+
+  const infoLink = infoState.visibility ? (
+    <a href={wikiInfo.link} id={`wikiSearch-${props.itemId}`} style={linkStyle}>
+      Wikipedia: {props.composer}
+    </a>
+  ) : null;
+
+  const infoButtonText = infoState.visibility
+    ? "Hide Composer Info"
+    : "Show Composer Info";
+
   return (
     <ItemDiv>
       <h3 style={titleStyle}>{props.title}</h3>
-      <p style={textStyle}><b>{"Composer: "}</b>
-        {props.composer},
-        <b>{" Style: "}</b>
+      <p style={textStyle}>
+        <b>{"Composer: "}</b>
+        {props.composer},<b>{" Style: "}</b>
         {props.style}
       </p>
       <Button
@@ -46,20 +99,14 @@ function LibraryItem(props) {
         id={props.itemId}
         style={removeStyle}
       ></Button>
-      <Button 
-        name={"Check Composer Info >>"} 
-        color={"#0c5460"} 
+      <Button
+        name={infoButtonText}
+        color={"#0c5460"}
         id={`btn-info-${props.itemId}`}
         style={infoStyle}
-        clicked={props.toggleInfo}
+        clicked={toggleInfoState}
       ></Button>
-      <a 
-        href={props.linkRef} 
-        id={`wikilink-${props.itemId}`}
-        style={linkStyle}
-      >
-        Wikipedia: {props.composer}
-      </a>
+      {infoLink}
     </ItemDiv>
   );
 }
--- src/Components/LibraryList.js
+++ src/Components/LibraryList.js
@@ -2,52 +2,17 @@ import React from "react";
 import LibraryItem from "./LibraryItem";
 
 const LibraryList = (props) => {
-  
-  
-  const getData = (searchTerm) => {
-    const respLink = {link: "#"};  
-    let url = "https://en.wikipedia.org/w/api.php";
-
-    const params = {
-      action: "opensearch",
-      search: searchTerm,
-      limit: "15",
-      namespace: "0",
-      format: "json"
-    };
-    
-    url = url + "?origin=*";
-
-    for (let key in params) {
-      url += `&${key}=${params[key]}`
-    }
-    
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {respLink.link = data[3][0]})
-      .catch((error) => console.log(error));
-
-      console.log(respLink.link)
-
-    return respLink.link
-;
-  }
-
-  return ( 
-    props.libraryList.map((item) => (
-      <LibraryItem
-        title={item.title}
-        composer={item.composer}
-        style={item.style}
-        key={item.id}
-        childColor={"#dc3545"}
-        removeClick={item.removeLibraryItem}
-        itemId={item.id}
-        // toggleInfo={}
-        linkRef={getData(item.composer)}
-      />
-    ))
-  )    
-}
+  return props.libraryList.map((item) => (
+    <LibraryItem
+      title={item.title}
+      composer={item.composer}
+      style={item.style}
+      key={item.id}
+      childColor={"#dc3545"}
+      removeClick={item.removeLibraryItem}
+      itemId={item.id}
+    />
+  ));
+};
 
 export default LibraryList;
